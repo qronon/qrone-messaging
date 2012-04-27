76ad3c545b044eb5aa0c2e44dcab3e118cb214a3
qronon <qronon@qrone.org>
2012-04-27 20:31:25 +0900

    initial commit

---
+++ src/org/qrone/messaging/QrONEMessagingClientConn.java
@@ -0,0 +1,63 @@
+package org.qrone.messaging;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.qrone.xmlsocket.XMLSocket;
+import org.qrone.xmlsocket.event.XMLSocketListener;
+import org.w3c.dom.Document;
+
+public class QrONEMessagingClientConn implements XMLSocketListener{
+	
+	private QrONEMessagingServer server;
+	private XMLSocket xmlsocket;
+	
+	public QrONEMessagingClientConn(QrONEMessagingServer server, XMLSocket xmlsocket) {
+		this.server = server;
+		this.xmlsocket = xmlsocket;
+	}
+
+	@Override
+	public void onConnect(boolean success) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onClose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onError(Exception e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTimeout() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onData(String data) {
+		try {
+			JSONObject obj = new JSONObject(data);
+			
+			String target = obj.getString("target");
+			
+			Set<QrONEMessagingClientConn> set = server.getTargets(target);
+			for (Iterator<QrONEMessagingClientConn> iter = set.iterator(); iter.hasNext();) {
+				iter.next().xmlsocket.send(data);
+			}
+			
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/org/qrone/messaging/QrONEMessagingServer.java
@@ -0,0 +1,78 @@
+package org.qrone.messaging;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
+
+import org.qrone.xmlsocket.XMLSocket;
+import org.qrone.xmlsocket.XMLSocketServer;
+import org.qrone.xmlsocket.event.XMLSocketListener;
+import org.qrone.xmlsocket.event.XMLSocketServerListener;
+import org.w3c.dom.Document;
+
+public class QrONEMessagingServer implements XMLSocketServerListener{
+
+	public static final int SERVER_PORT = 9645;
+	
+	private XMLSocketServer socketServer;
+	private Map<String, Set<QrONEMessagingClientConn>> map = new HashMap<String, Set<QrONEMessagingClientConn>>();
+	
+	public QrONEMessagingServer(){
+		
+		try {
+			socketServer = new XMLSocketServer();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		socketServer.setEncoding("UTF-8");
+		socketServer.addXMLSocketServerListener(this);
+		socketServer.open(SERVER_PORT);
+	}
+	
+	public void listen(int port){
+		socketServer.open(SERVER_PORT);
+	}
+
+	@Override
+	public void onOpen(boolean success) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onClose() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onError(Exception e) {
+		// TODO Auto-generated method stub
+	}
+	
+	public Set<QrONEMessagingClientConn> getTargets(String groupId){
+		return map.get(groupId);
+	}
+	
+	public void setTarget(String groupId, QrONEMessagingClientConn conn){
+		Set<QrONEMessagingClientConn> set = map.get(groupId);
+		if(set == null){
+			set = new HashSet<QrONEMessagingClientConn>();
+			map.put(groupId, set);
+		}
+		
+		set.add(conn);
+	}
+
+	@Override
+	public void onNewClient(XMLSocket xmlsocket) {
+		QrONEMessagingClientConn conn = new QrONEMessagingClientConn(this, xmlsocket);
+	}
+		
+	public static void main(String[] args){
+		QrONEMessagingServer server = new QrONEMessagingServer();
+		server.listen(SERVER_PORT);
+	}
+
+}
